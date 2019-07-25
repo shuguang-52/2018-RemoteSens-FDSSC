c924ea06c7853bf4e66e04c115fd51e0441500ff
Shuguang Dou <33367425+shuguang-52@users.noreply.github.com>
2019-07-25 14:43:28 +0800

    Create color_map.py

---
+++ classification_maps/color_map.py
@@ -0,0 +1,66 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def list_to_colormap(x_list):
+    y = np.zeros((x_list.shape[0], 3))
+    for index, item in enumerate(x_list):
+        if item == 0:
+            y[index] = np.array([255, 0, 0]) / 255.
+        if item == 1:
+            y[index] = np.array([0, 255, 0]) / 255.
+        if item == 2:
+            y[index] = np.array([0, 0, 255]) / 255.
+        if item == 3:
+            y[index] = np.array([255, 255, 0]) / 255.
+        if item == 4:
+            y[index] = np.array([0, 255, 255]) / 255.
+        if item == 5:
+            y[index] = np.array([255, 0, 255]) / 255.
+        if item == 6:
+            y[index] = np.array([192, 192, 192]) / 255.
+        if item == 7:
+            y[index] = np.array([128, 128, 128]) / 255.
+        if item == 8:
+            y[index] = np.array([128, 0, 0]) / 255.
+        if item == 9:
+            y[index] = np.array([128, 128, 0]) / 255.
+        if item == 10:
+            y[index] = np.array([0, 128, 0]) / 255.
+        if item == 11:
+            y[index] = np.array([128, 0, 128]) / 255.
+        if item == 12:
+            y[index] = np.array([0, 128, 128]) / 255.
+        if item == 13:
+            y[index] = np.array([0, 0, 128]) / 255.
+        if item == 14:
+            y[index] = np.array([255, 165, 0]) / 255.
+        if item == 15:
+            y[index] = np.array([255, 215, 0]) / 255.
+        if item == 16:
+            y[index] = np.array([0, 0, 0]) / 255.
+
+    return y
+
+
+def classification_map(map, height, width, dpi, savePath):
+    fig = plt.figure(frameon=False)
+    fig.set_size_inches(width*2.0/dpi, height*2.0/dpi)
+
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
+    fig.add_axes(ax)
+
+    ax.imshow(map)
+    fig.savefig(savePath, dpi=dpi)
+
+    return 0
+
+
+for color_num in range(17):
+    UP = np.array([color_num, color_num, color_num])
+    UP_color = list_to_colormap(UP)
+    y_re = np.reshape(UP_color, (1, 3, 3))
+    classification_map(y_re, 20, 60, 64, str(color_num) + '.png')
